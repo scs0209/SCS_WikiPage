696267f0ffc7a57e36abe98ac091d207b666bb51
scs0209 <110822847+scs0209@users.noreply.github.com>
2024-01-25 22:45:03 +0900

    delete: 'NoticeDetailContent' 컴포넌트에서 불필요한 코드 제거
    - 'NoticeDetailContent' 컴포넌트에서 아이템을 로컬스토리지에 저장하는 로직을 제거
    - 더 이상 사용하지 않는 'useEffect'와 'saveToLocalStorage'를 제거

--- Components/NoticeDetailContent.tsx
+++ Components/NoticeDetailContent.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { Item } from "@/lib/interface";
-import {
-  loadFromLocalStorage,
-  saveToLocalStorage,
-} from "@/lib/utils/localStorageUtils";
-import { useEffect, useState } from "react";
+import { loadFromLocalStorage } from "@/lib/utils/localStorageUtils";
+import { useState } from "react";
 
 interface Props {
   id: number;
@@ -14,10 +11,6 @@ interface Props {
 const NoticeDetailContent = ({ id }: Props) => {
   const [items, setItems] = useState(loadFromLocalStorage());
   const item = items.find((item: Item) => item.id === Number(id));
-
-  useEffect(() => {
-    saveToLocalStorage(items);
-  }, [items]);
 
   return (
     <div className="flex flex-col">
